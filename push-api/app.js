8ed6d71df4f7c5749d3f5e8fb973821e34793bc7
natewise <32283998+natewise@users.noreply.github.com>
2019-08-13 14:47:57 -0700

    Add files via upload

--- push-api/app.js
+++ push-api/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: 'vars.env' });
 const express = require('express');
 const webpush = require('web-push');
 const bodyParser = require('body-parser');
@@ -8,24 +9,36 @@ const app = express();
 app.use(express.static(path.join(__dirname, "client")));
 app.use(bodyParser.json());
 
-const publicKey = 'BDaN5wuHsjiFedtmmm_LcwtqzipDxZHWySVh4Sfez167_dTmdGPQpcb4qSIVHENXqPIhglRnfzLvEboMEOKoeII';
-const privateKey = 'OWrBRsOPjOCxxbnjzj2cVB7ZCy5UhkNkBNtwnCpE8z4';
-//generated through command 'web-push generate-vapid-keys'
-
+const publicKey = process.env.PUBLIC_VAPID_KEY; //generated through command 'web-push generate-vapid-keys'
+const privateKey = process.env.PRIVATE_VAPID_KEY;
+const notification = {
+    title: "Peak Time Hours Begin Soon",
+    body: "Click to begin cooling your home. ",
+    icon: "https://i.imgur.com/GDHivYy.png"
+};
 webpush.setVapidDetails( //Connect to web-push API with generated keys
     "mailto:test@test.com",
     publicKey,
     privateKey
 );
-app.post('/subscribe', (req,res)=>{ //Awaits valid subscription. This is where the actual interfacing is done with the web-push API. 
-    const subscription = req.body; 
-    console.log(subscription);
-    const notification = {
-        title: "Notification",
-        body: "You have been notified",
-        icon: "https://i.imgur.com/GDHivYy.png"
-    };
-    res.sendStatus(201);
-    webpush.sendNotification(subscription, JSON.stringify(notification)).catch((err) => console.error(err)); //Send notification to service workers. 
+app.get('/publickey', (req, res) => { //Send the publickey to the client
+    res.json({ publickey: publicKey });
 });
-app.listen(80, () => console.log('Server started on port 80'));
+app.post('/subscribe', (req, res) => { 
+    console.log(req.body);
+    const subscription = req.body.subscr;
+    //db.setSub(subscription, userdata); //persist the subscription and user data for later use. This will just be a simple call to whatever db we use
+    res.sendStatus(201);
+});
+app.post('/notify', (req, res) => { //Sends the recurring notifications. This is where the actual interfacing is done with the web-push API. 
+    const subscription = req.body.subscr; 
+    setTimeout(() => {
+        webpush.sendNotification(subscription, JSON.stringify(notification)).catch((err) => console.error(err)); //method that sends the notification to the service worker
+        setInterval(() => { //continuously notify at a set interval time
+            webpush.sendNotification(subscription, JSON.stringify(notification)).catch((err) => console.error(err));
+        }, 60000); //86,400,000 ms in 24 hours, 60000 ms in 1 minute
+    }, parseInt(req.body.msUntilTrigger));
+    console.log(subscription);
+    res.sendStatus(201);
+});
+app.listen(process.env.PORT, () => console.log('Server started on port %s', process.env.PORT));
--- push-api/client/client.js
+++ push-api/client/client.js
@@ -1,19 +1,60 @@
-const publicKey = 'BDaN5wuHsjiFedtmmm_LcwtqzipDxZHWySVh4Sfez167_dTmdGPQpcb4qSIVHENXqPIhglRnfzLvEboMEOKoeII';
+var publicKey;
+var subscription;
+var subscribeOptions;
+// if('serviceWorker' in navigator)
+//     subscribe().catch(err => console.log(err));
 
-if('serviceWorker' in navigator){ //I believe this is checking browser compatibility with serviceWorkers. Some browsers don't support them. 
-    send().catch(err => console.log(err));
+$.ajax({ //simple ajax call once the web page is first loaded. Retrieves publicKey from backend
+    url: '/publickey',
+    type: 'GET',
+    success: function (data) {
+        publicKey = data.publickey;
+        subscribeOptions = {
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(publicKey)
+        };
+    }
+});
+async function subscribe() { //Registers and subscribes service-worker.js to client's browser. Sends this subscription to the backend,
+                             //and tells the backend to begin the timer of when to send the web-push notification
+    if ('serviceWorker' in navigator) { //does the client's browser support service workers?
+        const register = await navigator.serviceWorker.register('/service-worker.js', { scope: "/" }) //Register local service worker on web browser with worker.js config file
+            .then((registration) => {
+                console.log("Service Worker successfully registered.")
+                return registration;
+            })
+            .catch((err) => { console.error(err) });
+        subscription = await register.pushManager.subscribe(subscribeOptions).then((pushSubscription) => { //Subscribe this service worker to the backend API so it can receive notifications/instructions
+            console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+            return pushSubscription;
+        });
+        await fetch('/subscribe', { //Persist subscription object in backend and begin the timer to trigger the notification
+            method: 'POST',
+            body: JSON.stringify({ subscr: subscription, username: $('#username').val() }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then((res) => {
+            beginTimer();
+        });
+    }
 }
-async function send(){
-    const register = await navigator.serviceWorker.register('/worker.js',{ //Register local service worker on web browser with worker.js config file
-        scope: '/' //worker only runs on main page
-    });
-    const subscription = await register.pushManager.subscribe({ //Subscribe this service worker to the backend API so it can receive notifications/instructions. 
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicKey)
-    });
-    await fetch('/subscribe', { //Send service worker subscription object to backend to connect worker to web-push API
-        method: 'POST',
-        body: JSON.stringify(subscription),
+async function beginTimer() {
+    var minutes = 60 - $('#minutes').val();
+    var currTime = new Date();
+    var timeTrigger = new Date(currTime);
+    timeTrigger.setHours(15, minutes, 0, 0); //Set the time to trigger the notification. 3:(minutes) pm e.g. 3:30
+    if (currTime > timeTrigger) {
+        timeTrigger.setDate(currTime.getDate() + 1); //if its already past peak time, offset the trigger time by one day
+    }
+    var msUntilTrigger = timeTrigger - currTime;
+    console.log("Triggering in %s seconds", msUntilTrigger / 1000);
+    //lines 43-54 take the user's input to calculate the time delay until when the user requested to be notified. Peak hours are
+    //assumed to begin at 4 everyday.  
+
+    await fetch('/notify', { //this is the route that tells the backend which service worker to trigger and how long to delay giving the notification. 
+        method: 'POST',      //this is the money route for this application
+        body: JSON.stringify({ subscr: subscription, msUntilTrigger: msUntilTrigger }),
         headers: {
             'content-type': 'application/json'
         }
@@ -22,12 +63,12 @@ async function send(){
 function urlBase64ToUint8Array(base64String) { //See web-push documentation
     const padding = "=".repeat((4 - base64String.length % 4) % 4)
     const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/")
-  
+
     const rawData = window.atob(base64)
     const outputArray = new Uint8Array(rawData.length)
-  
+
     for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i)
+        outputArray[i] = rawData.charCodeAt(i)
     }
     return outputArray
-  }
+}
